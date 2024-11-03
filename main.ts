2f0f0f9e70fc781dc9630a2a9d622b17ffaaad3e
RunningMan91 <93554915+RunningMan91@users.noreply.github.com>
2024-11-03 22:36:33 +1100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
 radio.onReceivedNumber(function (receivedNumber) {
+    basic.showNumber(receivedNumber)
     if (receivedNumber == 1001) {
+        basic.clearScreen()
         basic.showLeds(`
             # . . . .
             . . . . .
@@ -7,26 +9,30 @@ radio.onReceivedNumber(function (receivedNumber) {
             . . . . .
             . . . . #
             `)
-        basic.pause(200)
+        basic.pause(2000)
+        basic.clearScreen()
     } else {
+        basic.clearScreen()
         basic.showIcon(IconNames.SmallSquare)
         basic.pause(100)
         basic.showIcon(IconNames.Square)
         basic.pause(200)
         basic.showString("err")
         basic.showNumber(receivedNumber)
+        basic.clearScreen()
     }
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
     basic.showString("" + (UserID))
 })
 let UserID = 0
-UserID = 101
+UserID = 102
 radio.setGroup(UserID)
 basic.showString("" + (UserID))
+basic.clearScreen()
 basic.forever(function () {
     basic.showLeds(`
-        # . . . .
+        # # . . .
         . . . . .
         . . . . .
         . . . . .
