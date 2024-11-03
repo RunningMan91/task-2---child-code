85f1e0026b51bff7abf598a2a39d0bf4b9867186
RunningMan91 <93554915+RunningMan91@users.noreply.github.com>
2024-11-03 23:27:44 +1100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,22 +1,22 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    basic.showNumber(receivedNumber)
     if (receivedNumber == 1001) {
-        basic.clearScreen()
         basic.showLeds(`
-            # . . . .
+            # # . . .
             . . . . .
             . . . . .
             . . . . .
             . . . . #
             `)
-        basic.pause(2000)
-        basic.clearScreen()
+    } else if (receivedNumber == 1002) {
+        basic.showLeds(`
+            # # . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . # #
+            `)
     } else {
         basic.clearScreen()
-        basic.showIcon(IconNames.SmallSquare)
-        basic.pause(100)
-        basic.showIcon(IconNames.Square)
-        basic.pause(200)
         basic.showString("err")
         basic.showNumber(receivedNumber)
         basic.clearScreen()
